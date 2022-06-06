950bd4908ff2395285a8d17a9983d9d1f4a9d34b
Juan Pablo Correa <juanpablocorreapuerta@gmail.com>
2022-06-06 12:48:09 -0500

    Improvement hiding password with a button

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -7,13 +7,14 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-  final emailController = TextEditingController();
-  final passwordController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  var _passwordVisible = false;
 
   @override
   void dispose() {
-    emailController.dispose();
-    passwordController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
     super.dispose();
   }
 
@@ -27,21 +28,41 @@ class _LoginState extends State<Login> {
               height: 30,
             ),
             TextField(
-              controller: emailController,
+              controller: _emailController,
               textInputAction: TextInputAction.next,
               decoration: const InputDecoration(labelText: "Email"),
             ),
             const SizedBox(height: 5),
-            TextField(
-              controller: passwordController,
-              textInputAction: TextInputAction.next,
-              decoration: const InputDecoration(labelText: "Password"),
+            TextFormField(
+              keyboardType: TextInputType.text,
+              controller: _passwordController,
+              obscureText:
+                  !_passwordVisible, //This will obscure text dynamically
+              decoration: InputDecoration(
+                labelText: 'Password',
+                hintText: 'Enter your password',
+                // Here is key idea
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    // Based on passwordVisible state choose the icon
+                    _passwordVisible ? Icons.visibility : Icons.visibility_off,
+                    color: Theme.of(context).primaryColorDark,
+                  ),
+                  onPressed: () {
+                    // Update the state i.e. toogle the state of passwordVisible variable
+                    setState(() {
+                      _passwordVisible = !_passwordVisible;
+                    });
+                  },
+                ),
+              ),
             ),
             const SizedBox(height: 5),
             ElevatedButton.icon(
-              style: ElevatedButton.styleFrom(minimumSize: Size.fromHeight(50)),
-              icon: Icon(Icons.lock_open, size: 32),
-              label: Text("Sign In"),
+              style: ElevatedButton.styleFrom(
+                  minimumSize: const Size.fromHeight(50)),
+              icon: const Icon(Icons.lock_open, size: 32),
+              label: const Text("Sign In"),
               onPressed: signIn,
             )
           ],
@@ -51,8 +72,8 @@ class _LoginState extends State<Login> {
   Future signIn() async {
     try {
       await FirebaseAuth.instance.signInWithEmailAndPassword(
-          email: emailController.text.trim(),
-          password: passwordController.text.trim());
+          email: _emailController.text.trim(),
+          password: _passwordController.text.trim());
     } catch (e) {
       print(e);
     }
